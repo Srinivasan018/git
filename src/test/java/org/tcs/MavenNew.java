fab7c8fb9632f19e1f7425abeeda0aaffb926772
srinivasan098 <vasansri018@gmail.com>
2022-08-27 15:03:36 +0530

    1st Time Push

---
+++ src/test/java/org/tcs/MavenNew.java
@@ -0,0 +1,13 @@
+package org.tcs;
+
+public class MavenNew {
+
+	private void Method1() {
+		System.out.println("Method1");
+
+	}
+	private void Method2() {
+		System.out.println("Method2");
+
+	}
+}
